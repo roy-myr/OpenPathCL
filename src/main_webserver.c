74186392f224522b9d6a8f4d5824ab165ea86d09
Roy Meyer <128533154+roy-myr@users.noreply.github.com>
2024-11-10 20:35:41 +0100

    Added the ability to select the parallelizable algorithm in the frontend

--- src/main_webserver.c
+++ src/main_webserver.c
@@ -127,6 +127,8 @@ void handle_form_submission(int client_fd, char *request_body) {
     // Construct the full path to the executable
     if (strcmp(algorithm->valuestring, "parallel") == 0) {
         snprintf(full_program_path, PATH_MAX, "%s/OpenPathCL_parallel", cwd);
+    } else if (strcmp(algorithm->valuestring, "parallelizable") == 0) {
+        snprintf(full_program_path, PATH_MAX, "%s/OpenPathCL_parallelizable", cwd);
     } else if (strcmp(algorithm->valuestring, "serial") == 0) {
         snprintf(full_program_path, PATH_MAX, "%s/OpenPathCL_serial", cwd);
     } else {
